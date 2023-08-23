1326bffbd1fbef44567c0b9ba699f646349750b1
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-22 20:30:29 -0400

    👷 Adds Local Dev Wrapper for the Packaging Script

---
+++ scripts/package-dev.ps1
@@ -0,0 +1,42 @@
+#!/usr/bin/env pwsh
+# Take all the PowerShell module content in ../src and package it as a NuPkg. But only for local development purposes.
+param(
+)
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+
+[string] $gitversion_bin = $null
+if (Get-Command dotnet-gitversion -ErrorAction SilentlyContinue) {
+    $gitversion_bin = Get-Command dotnet-gitversion
+}
+elseif (Get-Command gitversion -ErrorAction SilentlyContinue) {
+    $gitversion_bin = Get-Command gitversion
+}
+else {
+    throw "Could not find gitversion commandline, please install it. See: https://gitversion.net/docs/usage/cli/installation."
+}
+
+[PSObject] $versionInfo = & $gitversion_bin | ConvertFrom-Json
+
+[string] $PackageVersionNuGet = $versionInfo | Select-Object -ExpandProperty NuGetVersionV2
+[string] $PackageVersionPrereleaseTag = $versionInfo | Select-Object -ExpandProperty PreReleaseTag
+[string] $PackageVersionMajorMinorPatchBuild = $versionInfo | Select-Object -ExpandProperty AssemblySemVer
+[string] $ReleaseNotes = "Fake Release Notes"
+[string] $CommitSha = $versionInfo | Select-Object -ExpandProperty Sha
+
+if (-not $PackageVersionPrereleaseTag) {
+    $PackageVersionNuGet += "-dev"
+    $PackageVersionPrereleaseTag = "dev"
+} else {
+    $PackageVersionNuGet = $PackageVersionNuGet.Replace("-" + $PackageVersionPrereleaseTag, "-dev-" + $PackageVersionPrereleaseTag)
+    $PackageVersionPrereleaseTag = "dev-" + $PackageVersionPrereleaseTag
+}
+
+$ds = [System.IO.Path]::DirectorySeparatorChar
+& "${PSScriptRoot}${ds}package.ps1" `
+    -PackageVersionNuGet $PackageVersionNuGet `
+    -PackageVersionMajorMinorPatchBuild $PackageVersionMajorMinorPatchBuild `
+    -PackageVersionPrereleaseTag $PackageVersionPrereleaseTag `
+    -ReleaseNotes $ReleaseNotes `
+    -CommitSha $CommitSha
