9a1c88801a32b69cebbb78666c3e1a71f7c68524
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-08-25 21:56:01 -0400

    ✨ Updates Module Implementation

--- src/poshy-wrap-python.psm1
+++ src/poshy-wrap-python.psm1
@@ -7,7 +7,7 @@ Get-ChildItem -Path "$PSScriptRoot/*.ps1" | ForEach-Object {
     . $_.FullName
 }
 
-if (Test-Command ipython3) {
+if ((Test-Command ipython3) -or $Global:PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED) {
     Set-Alias -Name ipy -Value ipython3
     Set-Alias -Name ipython -Value ipython3
 }
