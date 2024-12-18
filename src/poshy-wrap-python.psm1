25e9950689405a8b1f8d93621afaa56d985c66ca
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-18 13:59:17 -0500

    🏗️ Splits PSM1, Removes `#Requires` from PSM1
    Tests were failing because the module was not importable. The `#Requires` syntax being used in the root PSM1 prevented dependencies from being fully resolved before the module loading could complete, making the module unusable.

--- src/poshy-wrap-python.psm1
+++ src/poshy-wrap-python.psm1
@@ -1,16 +1,6 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
-#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
-Get-ChildItem -Path "$PSScriptRoot/*.ps1" | ForEach-Object {
-    . $_.FullName
-}
-
-if ((Test-Command ipython3) -or (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue)) {
-    Set-Alias -Name ipy -Value ipython3
-    Set-Alias -Name ipython -Value ipython3
-}
-
-Export-ModuleMember -Function * -Alias *
+. "$PSScriptRoot/poshy-wrap-python.ps1"
